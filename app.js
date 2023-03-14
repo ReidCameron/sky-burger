214ccad6585360b4fbb21899a2cbd8c52e9fd8dc
Cameron Reid <reidcj@reidcj-5520.rose-hulman.edu>
2023-03-14 08:07:18 -0500

    added netlify files and functions

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-const { Console } = require('console');
 const express = require ('express');
 const ejs = require('ejs');
-// const serverless = require('serverless-http');
+const serverless = require('serverless-http');
 const app = express();
 const path = require('path')
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-// app.set('views', path.join(__dirname, './public/views'));
-
 app.use('/',(req, res)=>{
     res.render('index');
 })
-app.listen(3000, ()=>{
-    console.log("listening on port 3000")
-})
+
 //serverless
-// const handler = serverless(app);
-// module.exports.handler = async (event, context) => {
-    // return await handler(event, context);
-// };
+const handler = serverless(app);
+module.exports.handler = async (event, context) => {
+    return await handler(event, context);
+};
